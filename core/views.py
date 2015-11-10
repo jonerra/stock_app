86c8c43cee86876064117e027149debeee52de6b
Jon <jonerra23@aim.com>
2015-11-10 20:23:58 +0000

    Add review detail

--- core/views.py
+++ core/views.py
@@ -25,16 +25,23 @@ class StockDetail(DetailView):
     model = Stock
     template_name = 'stock/stock_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(StockDetail, self).get_context_data(**kwargs)
+        stock = Stock.objects.get(id=self.kwargs['pk'])
+        reviews = Review.objects.filter(stock=stock)
+        context['reviews'] = reviews
+        return context
+
 class StockUpdate(UpdateView):
     model = Stock
     template_name = 'stock/stock_form.html'
     fields = ['symbol', 'company', 'description']
-    
+
 class StockDelete(DeleteView):
     model = Stock
     template_name = 'stock/stock_delete_form.html'
     success_url = reverse_lazy('stock_list')
-    
+
 class CreateReview(CreateView):
     model = Review
     template_name = "review/review_form.html"
@@ -42,7 +49,7 @@ class CreateReview(CreateView):
 
     def get_success_url(self):
         return self.object.stock.get_absolute_url()
-    
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.instance.stock = Stock.objects.get(id=self.kwargs['pk'])
