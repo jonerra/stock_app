e11c6128b8cf67f807410a1ca146bb16c246a7b6
Jon <jonerra23@aim.com>
2015-11-10 20:43:18 +0000

    add required user login

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
 from .views import *
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^user/', include('registration.backends.simple.urls')),
     url(r'^user/', include('django.contrib.auth.urls')),
-    url(r'^stock/create/$', StockPost.as_view(), name='stock_create'),
-    url(r'stock/$', StockList.as_view(), name='stock_list'),
-    url(r'^stock/(?P<pk>\d+)/$', StockDetail.as_view(), name='stock_detail'),
-    url(r'^stock/update/(?P<pk>\d+)/$', StockUpdate.as_view(), name='stock_update'),
-    url(r'^stock/delete/(?P<pk>\d+)/$', StockDelete.as_view(), name='stock_delete'),
-    url(r'^stock/(?P<pk>\d+)/review/create/$', CreateReview.as_view(), name='review_create'),
-    url(r'^stock/(?P<stock_pk>\d+)/review/update/(?P<review_pk>\d+)/$', UpdateReview.as_view(), name='review_update'),
-    url(r'^stock/(?P<stock_pk>\d+)/review/delete/(?P<review_pk>\d+)/$', DeleteReview.as_view(), name='review_delete'),
+    url(r'^stock/create/$', login_required(StockPost.as_view()), name='stock_create'),
+    url(r'stock/$', login_required(StockList.as_view()), name='stock_list'),
+    url(r'^stock/(?P<pk>\d+)/$', login_required(StockDetail.as_view()), name='stock_detail'),
+    url(r'^stock/update/(?P<pk>\d+)/$', login_required(StockUpdate.as_view()), name='stock_update'),
+    url(r'^stock/delete/(?P<pk>\d+)/$', login_required(StockDelete.as_view()), name='stock_delete'),
+    url(r'^stock/(?P<pk>\d+)/review/create/$', login_required(CreateReview.as_view()), name='review_create'),
+    url(r'^stock/(?P<stock_pk>\d+)/review/update/(?P<review_pk>\d+)/$', login_required(UpdateReview.as_view()), name='review_update'),
+    url(r'^stock/(?P<stock_pk>\d+)/review/delete/(?P<review_pk>\d+)/$', login_required(DeleteReview.as_view()), name='review_delete'),
 )
